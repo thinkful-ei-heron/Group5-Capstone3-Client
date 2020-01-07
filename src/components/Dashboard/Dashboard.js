408568284ef0a8ed41799813679c68e8a3eb3bc9
Zacharia Lutz <zacharia.lutz@gmail.coom>
2020-01-07 11:43:13 -0600

    starting basic wireframe

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,7 +4,17 @@ import './Dashboard.css';
 function Dashboard() {
   return (
     <section>
-
+      <form>
+        <label>Preview Images:</label>
+        <input type='checkbox' name='previewImg' checked />
+        <label>Extra Panel:</label>
+        <input type='checkbox' name='extraPanel' checked />
+        <label>Autosave:</label>
+        <input type='checkbox' name='autosave' />
+        <label>Interface Color:</label>
+        <input type='color' name='colorUI' value='#ffffff' />
+        <input type='submit' value='Save Changes' />
+      </form>
     </section>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 function Header() {
   return (
     <header>
-
+      <h1>Bookmark Organizer</h1>
     </header>
   );
 }
