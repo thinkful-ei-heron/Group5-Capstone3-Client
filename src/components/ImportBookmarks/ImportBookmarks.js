bed82f38117923bbfd803e975d2849dc9fafd1b3
zkhin <arandomname@gmail.com>
2020-01-08 16:43:23 -0500

    working import bookmarks html and parse

---
+++ src/components/ImportBookmarks/ImportBookmarks.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import './ImportBookmarks.css'
+import 'fs'
+import bmParser from '../../helpers/bookmarks-parser'
+
+
+export default class ImportBookmarks extends Component {
+
+  static defaultProps = {
+    storeBookmarks: () => { }
+  }
+
+  state = {
+    imported: false,
+    bookmarks: null,
+  }
+
+  handleImport = (e) => {
+    let reader = new FileReader()
+    reader.onload = (ev) => {
+      bmParser(reader.result, (err, res) => {
+        return this.setState({ bookmarks: res })
+      })
+    }
+    try {
+      reader.readAsText(e.target.files[0])
+    } catch {
+      this.setState({ error: 'No valid file' })
+      return
+    }
+  }
+
+  render() {
+    return (
+      <div className="Import">
+        <form id="importform" className="ImportForm">
+          <fieldset>
+            Upload your bookmarks HTML file
+            <label htmlFor="bookmarkfile">Bookmark File</label>
+            <input type="file" name="bookmarkfile" id="bookmarkfile" onChange={this.handleImport} />
+
+          </fieldset>
+        </form>
+      </div>
+    )
+  }
+}
--- src/helpers/parser-test.js
+++
@@ -1,5 +0,0 @@
-const path = require('path')
-const bmParser = require('bookmark-parser')
-const testFile = path.join(__dirname, '/bookmarks.html')
-
-bmParser.readFromHTMLFile(testFile).then(res=>console.log(res))
