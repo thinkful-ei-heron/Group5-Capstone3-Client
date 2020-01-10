2eb037ea7d21b8dd54e4d41cd995ef24b7cf7eb3
Zacharia Lutz <zacharia.lutz@gmail.coom>
2020-01-09 18:02:40 -0600

    set up basic login stub and UserContext

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,30 +1,56 @@
 import React from 'react';
+import UserContext from '../../contexts/UserContext';
 import './Login.css';
 
-function Login() {
-  return (
-    <form className='container'>
-      <label htmlFor='username'>Username:</label>
-      <br />
-      <input
-        type='text'
-        name='username' id='username'
-        required
-        autoFocus
-      />
-      <br />
-      <label htmlFor='password'>Password:</label>
-      <br />
-      <input
-        type='password'
-        name='password' id='password'
-        required
-      />
-      <br />
-      <input type='submit' value='Submit' className='btn' />
-      <input type='reset' value='Reset' className='btn' />
-    </form>
-  );
-}
+import AuthApiService from '../../services/auth-api-service';
 
-export default Login;
+export default class Login extends React.Component {
+  static contextType = UserContext;
+
+  state = { error: null }
+
+  submitHandler = ev => {
+    ev.preventDefault();
+    const { username, password } = ev.target
+
+    AuthApiService.postLogin({
+      username: username.value,
+      password: password.value,
+    })
+      .then(res => {
+        username.value = ''
+        password.value = ''
+        this.context.processLogin(res.authToken)
+        this.context.onLoginSuccess()
+      })
+      .catch(res => {
+        this.setState({ error: res.error })
+      })
+  }
+
+  render() {
+    return (
+      <form className='container' onSubmit={ev => this.submitHandler(ev)}>
+        <label htmlFor='username'>Username:</label>
+        <br />
+        <input
+          type='text'
+          name='username' id='username'
+          required
+          autoFocus
+        />
+        <br />
+        <label htmlFor='password'>Password:</label>
+        <br />
+        <input
+          type='password'
+          name='password' id='password'
+          required
+        />
+        <br />
+        <input type='submit' value='Submit' className='btn' />
+        <input type='reset' value='Reset' className='btn' />
+      </form>
+    );
+  }
+}
