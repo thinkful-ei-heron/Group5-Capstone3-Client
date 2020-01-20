220025ebd2af8fb5c1fdae6ae520b7df08529a71
Michael Kirsch <michaelskirsch@gmail.com>
2020-01-20 13:05:29 -0700

    added find/update node utils to bookmark context

--- src/contexts/BookmarkContext.js
+++ src/contexts/BookmarkContext.js
@@ -4,8 +4,8 @@ const BookmarkContext = React.createContext();
 export default BookmarkContext;
 export class BookmarkContextProvider extends React.Component {
   state = {
-		bookmarks: null,
-		flat: null,
+    bookmarks: null,
+    flat: null,
     error: null,
     selected1: null,
     selected2: null,
@@ -25,11 +25,11 @@ export class BookmarkContextProvider extends React.Component {
     this.setState({
       bookmarks
     });
-	};
+  };
 
-	setFlat = flat => {
-		this.setState({ flat });
-	};
+  setFlat = flat => {
+    this.setState({ flat });
+  };
 
   setListId = listId => {
     this.setState({ listId });
@@ -39,13 +39,47 @@ export class BookmarkContextProvider extends React.Component {
     this.setState({ listName });
   };
 
+  updateNode = (id, updateInfo) => {
+    const bookmarks = [...this.state.bookmarks];
+    const node = this.findNodeById(id, bookmarks);
+    if (!node) throw new Error('could not find matching node');
+    for (const key of Object.keys(updateInfo)) {
+      node[key] = updateInfo[key];
+    }
+    this.setState({ bookmarks });
+  };
+
+  findNodeById = (id, nodes = this.state.bookmarks) => {
+    console.log('find by id: ', id, ',', nodes);
+    return this._recursiveFind(this._makeIdPredicate(id), nodes);
+  };
+
+  //given id, return predicate function that examines a node to see if node.id is equal to the provided id
+  _makeIdPredicate = id => node => node.id === id;
+
+  _recursiveFind(predicate, nodes) {
+    for (const node of nodes) {
+      console.log(node);
+      if (predicate(node)) {
+        return node;
+      }
+      if (node.contents) {
+        const maybeResult = this._recursiveFind(predicate, node.contents);
+        if (maybeResult) return maybeResult;
+      }
+    }
+    return null;
+  }
+
   render() {
     const value = {
       ...this.state,
       setBookmarks: this.setBookmarks,
       setListId: this.setListId,
-			setListName: this.setListName,
-			setFlat: this.setFlat,
+      setListName: this.setListName,
+      setFlat: this.setFlat,
+      updateNode: this.updateNode,
+      findNodeById: this.findNodeById
     };
     return (
       <BookmarkContext.Provider value={value}>
