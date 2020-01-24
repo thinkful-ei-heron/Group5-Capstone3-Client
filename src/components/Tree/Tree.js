3b310382f181c8e19f420a0d1e32a32ef4bb7c40
zkhin <arandomname@gmail.com>
2020-01-24 11:55:17 -0500

    final fix

--- src/components/Tree/Tree.js
+++ src/components/Tree/Tree.js
@@ -92,7 +92,9 @@ export default class Tree extends Component {
             onDrop={this.toggleSelect}
             onDragOver={(e) => { e.preventDefault() }}
           >
-            {this.props.data.icon !== 'undefined' || this.props.data.icon !== null &&
+            {
+              this.props.data.icon &&
+              this.props.data.icon !== 'undefined' &&
               this.props.data.type &&
               this.props.data.type !== 'folder' &&
               <img className='Tree-icon' src={this.props.data.icon} alt='icon' />}
